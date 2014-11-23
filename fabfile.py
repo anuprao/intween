e179ce27ac5d60bf8f9b911c88058eea54fc62be
Anup Jayapal Rao <anup.kadam@gmail.com>
2014-11-23 20:28:13 +0530

    Added BSD License to repository, Added List of Contributors Added a fabric fab file to automate builds

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+from fabric.api import *
+import fabric.contrib.project as project
+import os
+
+def clean():
+    print "Cleaning"
+
+def build():
+    print "Building"
+
+def gendocs():
+    print "Denerating Docs"
--- python/intween.py
+++ python/intween.py
@@ -4,28 +4,9 @@
 # which was based on caurina Tweener: http://code.google.com/p/tweener/
 # which was forked from pyTweener: http://wiki.python-ogre.org/index.php/CodeSnippits_pyTweener
 #
-# Copyright (c)
-#   Ben Harling (2009)
-#   Benjamin Woodruff (2010)
-#   Anup Jayapal Rao (2014)
+# Copyright (c) 2014, Refer accompanying contributors.txt file for authors.
+# Refer accompanying LICENSE file for the applicable licensing terms.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 import time
 import math
